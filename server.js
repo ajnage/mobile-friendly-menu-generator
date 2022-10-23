ee2a96f717c153a16fa65c5bec0c3655c151868b
Abhijeet Nage <abhijeet@ualberta.ca>
2022-10-22 21:10:30 -0600

    adding edit function to menu items

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
+const editRoutes = require("./routes/edit");
 
 //Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
@@ -58,6 +59,8 @@ app.use(express.static("assets"));
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
+app.use("/edit", editRoutes);
+
 
 //Server Running
 app.listen(process.env.PORT, () => {
