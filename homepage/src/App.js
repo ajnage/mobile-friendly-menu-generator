4a642bace7ec89921de4f7ca3866a480303c968f
Sava <savab2164@gmail.com>
2023-02-10 21:52:03 -0700

    edit

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,8 +1,88 @@
+import { motion } from "framer-motion";
 import "./App.css";
 import QR from "./QR.svg";
+import { useState } from "react";
 
 function AppButton() {
-  return <button className="testButton">DO YOU WANT TO CHECK IT OUT?</button>;
+  return (
+    <motion.div
+      className="testButton"
+      whileHover={{ scale: [null, 1.5, 1.4] }}
+      transition={{ duration: 0.3 }}
+    >
+      {" "}
+      Check it out!{" "}
+    </motion.div>
+  );
+}
+
+const itemVariants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+};
+
+function HomeMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <motion.nav
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      className="menu"
+    >
+      <motion.button
+        whileTap={{ scale: 0.97 }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Menu
+        <motion.div
+          variants={{
+            open: { rotate: 180 },
+            closed: { rotate: 0 },
+          }}
+          transition={{ duration: 0.2 }}
+          style={{ originY: 0.55 }}
+        >
+          <svg width="15" height="15" viewBox="0 0 20 20">
+            <path d="M0 7 L 20 7 L 10 16" />
+          </svg>
+        </motion.div>
+      </motion.button>
+      <motion.ul
+        variants={{
+          open: {
+            clipPath: "inset(0% 0% 0% 0% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.7,
+              delayChildren: 0.3,
+              staggerChildren: 0.05,
+            },
+          },
+          closed: {
+            clipPath: "inset(10% 50% 90% 50% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.3,
+            },
+          },
+        }}
+        style={{ pointerEvents: isOpen ? "auto" : "none" }}
+      >
+        <motion.li variants={itemVariants}>Item 1 </motion.li>
+        <motion.li variants={itemVariants}>Item 2 </motion.li>
+        <motion.li variants={itemVariants}>Item 3 </motion.li>
+        <motion.li variants={itemVariants}>Item 4 </motion.li>
+        <motion.li variants={itemVariants}>Item 5 </motion.li>
+      </motion.ul>
+    </motion.nav>
+  );
 }
 
 export default function App() {
@@ -13,7 +93,19 @@ export default function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={QR} className="App-logo" alt="logo" />
+        <HomeMenu />
+        <motion.img
+          src={QR}
+          className="App-logo"
+          alt="logo"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 2,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        />
         <h1 style={h1style}>Welcome to the menu-maker app!</h1>
         <AppButton />
       </div>
