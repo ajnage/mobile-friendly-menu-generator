cff613affc768fa9fbaed87f7af0e614b92b0fb9
Sava <savab2164@gmail.com>
2023-02-11 21:06:46 -0700

    edit

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import QR from "./QR.svg";
 import { useState } from "react";
 
+import Button from "@mui/material/Button";
+import StickyFooter from "./StickyFooter";
+
 function AppButton() {
   return (
     <motion.div
@@ -10,90 +13,24 @@ function AppButton() {
       whileHover={{ scale: [null, 1.5, 1.4] }}
       transition={{ duration: 0.3 }}
     >
-      {" "}
-      Check it out!{" "}
+      <Button variant="contained">Check it out</Button>
     </motion.div>
   );
 }
 
-const itemVariants = {
-  open: {
-    opacity: 1,
-    y: 0,
-    transition: { type: "spring", stiffness: 300, damping: 24 },
-  },
-  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
-};
-
-function HomeMenu() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <motion.nav
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-      className="menu"
-    >
-      <motion.button
-        whileTap={{ scale: 0.97 }}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        Menu
-        <motion.div
-          variants={{
-            open: { rotate: 180 },
-            closed: { rotate: 0 },
-          }}
-          transition={{ duration: 0.2 }}
-          style={{ originY: 0.55 }}
-        >
-          <svg width="15" height="15" viewBox="0 0 20 20">
-            <path d="M0 7 L 20 7 L 10 16" />
-          </svg>
-        </motion.div>
-      </motion.button>
-      <motion.ul
-        variants={{
-          open: {
-            clipPath: "inset(0% 0% 0% 0% round 10px)",
-            transition: {
-              type: "spring",
-              bounce: 0,
-              duration: 0.7,
-              delayChildren: 0.3,
-              staggerChildren: 0.05,
-            },
-          },
-          closed: {
-            clipPath: "inset(10% 50% 90% 50% round 10px)",
-            transition: {
-              type: "spring",
-              bounce: 0,
-              duration: 0.3,
-            },
-          },
-        }}
-        style={{ pointerEvents: isOpen ? "auto" : "none" }}
-      >
-        <motion.li variants={itemVariants}>Item 1 </motion.li>
-        <motion.li variants={itemVariants}>Item 2 </motion.li>
-        <motion.li variants={itemVariants}>Item 3 </motion.li>
-        <motion.li variants={itemVariants}>Item 4 </motion.li>
-        <motion.li variants={itemVariants}>Item 5 </motion.li>
-      </motion.ul>
-    </motion.nav>
-  );
-}
-
 export default function App() {
-  const h1style = {
-    color: "white",
-    left: "30px",
-  };
+  const h1style = { color: "white", left: "30px" };
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
       <div className="App-header">
-        <HomeMenu />
         <motion.img
           src={QR}
           className="App-logo"
@@ -113,6 +50,7 @@ export default function App() {
         This menu maker lets restaurant owners save the hassle of creating
         outdated, wasteful, and uninspiring menus
       </p>
+      <StickyFooter />
     </div>
   );
 }
