4f1559354b05dc89d1928dd77e5b5301aa44abb8
Sava-B <savab2164@gmail.com>
2023-05-27 12:05:10 -0600

    edit

--- frontend/src/routes/Dashboard.js
+++ frontend/src/routes/Dashboard.js
@@ -38,236 +38,222 @@ import { getRestaurantbyId, getRestaurants } from "../axios/API";
 // instead of using the array from dashboardstats.js
 
 const Dashboard = () => {
-	const [menusLeft, setMenusLeft] = useState(50);
+  const [menusLeft, setMenusLeft] = useState(50);
 
-	const [RevenueStats, setRevenueStats] = useState([]);
-	const [OrderStats, setOrderStats] = useState([]);
-	const [ClickStats, setClickStats] = useState([]);
+  const [RevenueStats, setRevenueStats] = useState([]);
+  const [OrderStats, setOrderStats] = useState([]);
+  const [ClickStats, setClickStats] = useState([]);
 
-	useEffect(() => {
-		getRestaurants()
-			.then(function (response) {
-				// handle success
-				console.log(response.data[1].restaurantName);
-			})
-			.catch(function (error) {
-				// handle error
-				console.log(error);
-			})
-			.finally(function () {
-				// always executed
-			});
+  useEffect(() => {
+    getRestaurants()
+      .then(function (response) {
+        // handle success
+        console.log(response.data[1].restaurantName);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
 
-		getRestaurantbyId("644dfe0d152ecd497e954fcf").then((data) =>
-			console.log(data)
-		);
-	}, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3434/api/revenue-stats/646eb3a8b30795e61bc2a0de")
+      .then(function (response) {
+        // handle success
+        console.log("2 revenue: ", response.data[2].revenue);
+        console.log("Response data.length: ", response.data.length);
+        const revenues = response.data.map(({ revenue }) => revenue);
+        console.log("Revenues: ", revenues);
+        const upToDateData = response.data.length - 1;
+        setRevenueStats(
+          RevenueStats.concat(response.data[upToDateData].revenue)
+        );
+        console.log("Revenue Stats: ", RevenueStats);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
 
-	useEffect(() => {
-		axios.get(
-			"http://localhost:3434/api/revenue-stats/646eb3a8b30795e61bc2a0de"
-		)
-			.then(function (response) {
-				// handle success
-				console.log("2 revenue: ", response.data[2].revenue);
-				console.log("Response data.length: ", response.data.length);
-				const revenues = response.data.map(
-					({ revenue }) => revenue
-				);
-				console.log("Revenues: ", revenues);
-				const upToDateData = response.data.length - 1;
-				setRevenueStats(
-					RevenueStats.concat(
-						response.data[upToDateData].revenue
-					)
-				);
-				console.log("Revenue Stats: ", RevenueStats);
-			})
-			.catch(function (error) {
-				// handle error
-				console.log(error);
-			})
-			.finally(function () {
-				// always executed
-			});
-	}, []);
+  const [RevenueChartData, setRevenueChartData] = useState({
+    labels: RevenueStats.map((data) => data.revenue),
+    datasets: [
+      {
+        label: "Revenue",
+        data: RevenueStats.map((data) => data.revenue),
+        title: RevenueStats.title,
+        backgroundColor: ["#7a2c2c"],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+  const [orderChartData, setOrderChartData] = useState({
+    labels: OrderStats.map((data) => data.month),
+    datasets: [
+      {
+        label: "Orders",
+        data: OrderStats.map((data) => data.orders),
+        title: OrderStats.title,
+        backgroundColor: ["#7a2c2c"],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
 
-	const [RevenueChartData, setRevenueChartData] = useState({
-		labels: RevenueStats.map((data) => data.revenue),
-		datasets: [
-			{
-				label: "Revenue",
-				data: RevenueStats.map((data) => data.revenue),
-				title: RevenueStats.title,
-				backgroundColor: ["#7a2c2c"],
-				borderColor: "black",
-				borderWidth: 2,
-			},
-		],
-	});
-	const [orderChartData, setOrderChartData] = useState({
-		labels: OrderStats.map((data) => data.month),
-		datasets: [
-			{
-				label: "Orders",
-				data: OrderStats.map((data) => data.orders),
-				title: OrderStats.title,
-				backgroundColor: ["#7a2c2c"],
-				borderColor: "black",
-				borderWidth: 2,
-			},
-		],
-	});
+  const [clickChartData, setClickChartData] = useState({
+    labels: ClickStats.map((data) => data.month),
+    datasets: [
+      {
+        label: "Clicks",
+        data: ClickStats.map((data) => data.clicks),
+        title: ClickStats.title,
+        backgroundColor: [
+          "#7a2c2c",
+          "teal",
+          "white",
+          "black",
+          "grey",
+          "darkgrey",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
 
-	const [clickChartData, setClickChartData] = useState({
-		labels: ClickStats.map((data) => data.month),
-		datasets: [
-			{
-				label: "Clicks",
-				data: ClickStats.map((data) => data.clicks),
-				title: ClickStats.title,
-				backgroundColor: [
-					"#7a2c2c",
-					"teal",
-					"white",
-					"black",
-					"grey",
-					"darkgrey",
-				],
-				borderColor: "black",
-				borderWidth: 2,
-			},
-		],
-	});
+  // Here the user variable is just the username of the restaurant owner, currently it is
+  // set to 'owner' before someone signs in!
+  const user = `Owner`;
+  return (
+    <Container sx={{ bgcolor: "primary.grey", width: "100vw", height: "auto" }}>
+      <Typography
+        variant="h1"
+        align="center"
+        color="primary.white"
+        gutterBottom
+        sx={{ pt: 5, pb: 5 }}
+        fontWeight={"bold"}
+      >
+        {" "}
+        {user}'s Dashboard
+      </Typography>
+      <Typography variant="h3" align="center" sx={{ mb: "10vh" }}>
+        General Statistics
+      </Typography>
+      <Paper
+        elevation={20}
+        sx={{
+          height: "45vh",
+          width: "auto",
+          display: "flex",
+          flexDirection: "row",
+          flexGrow: "4",
+          flexWrap: "wrap",
+          pl: "1vw",
+          pr: "1vw",
+          ml: "-7.5vw",
+          justifyContent: "space-between",
+          alignSelf: "center",
+          bgcolor: "primary.verydark",
+          overflow: "hidden",
+          mb: "100px",
+        }}
+      >
+        <Paper elevation={10} sx={{ height: "20vh", mt: "12vh", ml: "7vw" }}>
+          <BarChart chartData={RevenueChartData} />{" "}
+        </Paper>
 
-	// Here the user variable is just the username of the restaurant owner, currently it is
-	// set to 'owner' before someone signs in!
-	const user = `Owner`;
-	return (
-		<Container
-			sx={{ bgcolor: "primary.grey", width: "100vw", height: "auto" }}
-		>
-			<Typography
-				variant="h1"
-				align="center"
-				color="primary.white"
-				gutterBottom
-				sx={{ pt: 5, pb: 5 }}
-				fontWeight={"bold"}
-			>
-				{" "}
-				{user}'s Dashboard
-			</Typography>
-			<Typography variant="h3" align="center" sx={{ mb: "10vh" }}>
-				General Statistics
-			</Typography>
-			<Paper
-				elevation={20}
-				sx={{
-					height: "45vh",
-					width: "auto",
-					display: "flex",
-					flexDirection: "row",
-					flexGrow: "4",
-					flexWrap: "wrap",
-					pl: "1vw",
-					pr: "1vw",
-					ml: "-7.5vw",
-					justifyContent: "space-between",
-					alignSelf: "center",
-					bgcolor: "primary.verydark",
-					overflow: "hidden",
-					mb: "100px",
-				}}
-			>
-				<Paper
-					elevation={10}
-					sx={{ height: "20vh", mt: "12vh", ml: "7vw" }}
-				>
-					<BarChart chartData={RevenueChartData} />{" "}
-				</Paper>
+        <Paper elevation={10} sx={{ height: "20vh", mt: "12vh" }}>
+          <BarChart chartData={orderChartData} />{" "}
+        </Paper>
 
-				<Paper elevation={10} sx={{ height: "20vh", mt: "12vh" }}>
-					<BarChart chartData={orderChartData} />{" "}
-				</Paper>
-
-				<Paper
-					elevation={10}
-					sx={{
-						height: "20vh",
-						mt: "12vh",
-						mr: "7vw",
-						width: "12vw",
-					}}
-				>
-					<Typography
-						variant="h5"
-						align="center"
-						sx={{ m: "10", color: "primary.verydark" }}
-					>
-						Menus left
-					</Typography>
-					<Typography
-						variant="h1"
-						fontWeight={"bold"}
-						align="center"
-						sx={{ m: "10", color: "primary.verydark" }}
-					>
-						{menusLeft}{" "}
-					</Typography>
-					<Button
-						variant="contained"
-						sx={{ mt: "2vh", display: "center" }}
-						onClick={() => {
-							setMenusLeft(menusLeft - 1);
-						}}
-						component={Link}
-						to={"../menu_builder"}
-					>
-						Make Menu
-					</Button>
-				</Paper>
-			</Paper>
-			<Paper
-				elevation={20}
-				align="center"
-				sx={{
-					height: "30vh",
-					width: "auto",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					alignContent: "space-around",
-					flexWrap: "wrap",
-					pl: "1vw",
-					pr: "1vw",
-					ml: "-7.5vw",
-					bgcolor: "primary.verydark",
-					overflow: "hidden",
-					mb: "100px",
-				}}
-			>
-				<Typography variant="h3" sx={{ mt: "-20" }}>
-					{" "}
-					Extra{" "}
-				</Typography>
-				<Paper
-					sx={{
-						my: 10,
-						align: "center",
-						position: "relative",
-						height: "20vh",
-						width: "20vw",
-					}}
-				>
-					<LineChart
-						chartData={clickChartData}
-						options={{ maintainAspectRatio: false }}
-					/>
-				</Paper>
-			</Paper>
-		</Container>
-	);
+        <Paper
+          elevation={10}
+          sx={{
+            height: "20vh",
+            mt: "12vh",
+            mr: "7vw",
+            width: "12vw",
+          }}
+        >
+          <Typography
+            variant="h5"
+            align="center"
+            sx={{ m: "10", color: "primary.verydark" }}
+          >
+            Menus left
+          </Typography>
+          <Typography
+            variant="h1"
+            fontWeight={"bold"}
+            align="center"
+            sx={{ m: "10", color: "primary.verydark" }}
+          >
+            {menusLeft}{" "}
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{ mt: "2vh", display: "center" }}
+            onClick={() => {
+              setMenusLeft(menusLeft - 1);
+            }}
+            component={Link}
+            to={"../menu_builder"}
+          >
+            Make Menu
+          </Button>
+        </Paper>
+      </Paper>
+      <Paper
+        elevation={20}
+        align="center"
+        sx={{
+          height: "30vh",
+          width: "auto",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignContent: "space-around",
+          flexWrap: "wrap",
+          pl: "1vw",
+          pr: "1vw",
+          ml: "-7.5vw",
+          bgcolor: "primary.verydark",
+          overflow: "hidden",
+          mb: "100px",
+        }}
+      >
+        <Typography variant="h3" sx={{ mt: "-20" }}>
+          {" "}
+          Extra{" "}
+        </Typography>
+        <Paper
+          sx={{
+            my: 10,
+            align: "center",
+            position: "relative",
+            height: "20vh",
+            width: "20vw",
+          }}
+        >
+          <LineChart
+            chartData={clickChartData}
+            options={{ maintainAspectRatio: false }}
+          />
+        </Paper>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Dashboard;
