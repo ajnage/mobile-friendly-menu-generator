7c67837aeb35d7d08f2b1b1a8e87aa999865b685
Sava <savab2164@gmail.com>
2023-06-08 20:30:32 -0600

    slight changes to remove errors

--- client/src/axios/API.js
+++ client/src/axios/API.js
@@ -10,8 +10,14 @@ const API = axios.create({
 });
 
 // REST API
-export const getRestaurants = (email) => API.post(`/restaurants`, email);
+export const getRestaurants = () => API.get("/restaurants");
 export const getRestaurantbyId = (id) => API.get(`/restaurants/${id}`);
+export const getRevenuebyId = (id) => API.get(`/revenue-stats/${id}`);
+export const getOrdersbyId = (id) => API.get(`/order-stats/${id}`);
+export const getClicksbyId = (id) => API.get(`/click-stats/${id}`);
+export const getItems = () => API.get(`/items`);
+export const postItems = (data) => API.post(`/items/create-item`, { name: data.name, desc: data.desc, price: data.price, image: data.image, category: data.category })
+export const updateCategoryForm = (category) => API.get(`/items/${category}`);
 
 export const login = (token) =>
 	API.post(
