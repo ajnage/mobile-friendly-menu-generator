0da18ab543c5ec5a4c1d3c80873d682443919674
Sava <savab2164@gmail.com>
2023-06-21 21:21:07 -0600

    edit

--- server/controllers/userAuth.js
+++ server/controllers/userAuth.js
@@ -1,53 +1,5 @@
 module.exports = {
-<<<<<<< HEAD
-	login: async (req, res) => {
-		const options = {
-			method: "POST",
-			url: `${process.env.AUTH_DOMAIN}/oauth/token`,
-			headers: { "content-type": "application/x-www-form-urlencoded" },
-			data: new URLSearchParams({
-				grant_type: "client_credentials",
-				client_id: process.env.CLIENT_API_ID,
-				client_secret: process.env.CLIENT_API_SECRET,
-				audience: `${process.env.AUTH_DOMAIN}/api/v2/`,
-			}),
-		};
-
-		const access_token = await axios
-			.request(options)
-			.then(function (response) {
-				return response.data.access_token;
-			})
-			.catch(function (error) {
-				console.error(error);
-			});
-
-		try {
-			const response = await axios.get(
-				`${process.env.AUTH_DOMAIN}/api/v2/users/${req.auth.sub}`,
-				{
-					headers: {
-						Authorization: `Bearer ${access_token}`,
-					},
-				}
-			);
-
-			const userProfile = response.data;
-			console.log('Userprofile:', userProfile);
-			const { email } = userProfile;
-
-			const isUser = await User.findOne({ email });
-
-			if (isUser) return res.status(200).send();
-
-			await User.create({ email });
-			res.status(201).send();
-		} catch (error) {
-			console.log(error);
-		}
-=======
 	auth: async (req, res, next) => {
 		res.status(200).send();
->>>>>>> 41fa8afd7901f2c02ef20879999f947349bf010e
 	},
 };
